c41c743e88732cb354d72a2ed2add4b9c492c62c
MattOfficial <chand.matruprasad@outlook.com>
2021-03-17 16:25:53 +0530

    Bottom navigation designed

--- navigation/MealsNavigator.tsx
+++ navigation/MealsNavigator.tsx
@@ -1,14 +1,20 @@
 import * as React from "react";
-import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
 import { StyleSheet, Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import Colors from "../constants/Colors";
+import FavouritesScreen from "../screens/Favourites";
 
 const MealsNavigator = createStackNavigator();
+const TabsNavigator = createBottomTabNavigator();
+const TabsNavigatorAndroid = createMaterialBottomTabNavigator();
 
-export default function MealStack() {
+const MealStackNavigator = () => {
   return (
     <MealsNavigator.Navigator
       screenOptions={{
@@ -33,6 +39,74 @@ export default function MealStack() {
       />
     </MealsNavigator.Navigator>
   );
+};
+
+const IosNav = (
+  <TabsNavigator.Navigator
+    initialRouteName="Home"
+    tabBarOptions={{ activeTintColor: Colors.accentColor }}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        if (route.name === "Home") {
+          return <Ionicons name="ios-restaurant" size={size} color={color} />;
+        } else if (route.name === "Favourites") {
+          return (
+            <Ionicons
+              name="md-heart-sharp"
+              size={size}
+              color={focused ? "red" : "grey"}
+            />
+          );
+        }
+      },
+    })}
+  >
+    <TabsNavigator.Screen name="Home" component={MealStackNavigator} />
+    <TabsNavigator.Screen name="Favourites" component={FavouritesScreen} />
+  </TabsNavigator.Navigator>
+);
+
+const AndroidNav = (
+  <TabsNavigatorAndroid.Navigator
+    shifting={true}
+    initialRouteName="Home"
+    barStyle={{ backgroundColor: Colors.accentColor }}
+  >
+    <TabsNavigatorAndroid.Screen
+      name="Home"
+      component={MealStackNavigator}
+      options={{
+        tabBarLabel: "",
+        tabBarIcon: ({ focused, color }) => (
+          <Ionicons
+            name="ios-restaurant"
+            size={25}
+            color={focused ? "white" : color}
+          />
+        ),
+        tabBarColor: Colors.primaryColor,
+      }}
+    />
+    <TabsNavigatorAndroid.Screen
+      name="Favourites"
+      component={FavouritesScreen}
+      options={{
+        tabBarLabel: "",
+        tabBarIcon: ({ focused, color }) => (
+          <Ionicons
+            name="md-heart-sharp"
+            size={25}
+            color={focused ? "white" : color}
+          />
+        ),
+        tabBarColor: Colors.accentColor,
+      }}
+    />
+  </TabsNavigatorAndroid.Navigator>
+);
+
+export default function MealStack() {
+  return Platform.OS === "ios" ? IosNav : AndroidNav;
 }
 
 const styles = StyleSheet.create({
